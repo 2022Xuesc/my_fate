90ee4508c95e5981bda5c28f580676921a82a8ef
2022Xuesc <22S051026@stu.hit.edu.cn>
2023-03-22 19:02:05 +0800

    update

--- my_practice/draw_figures/draw_heatmap.py
+++ my_practice/draw_figures/draw_heatmap.py
@@ -1,9 +1,13 @@
+import os.path
+
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 import pandas as pd
+
+from my_practice.draw_figures.labels_cnts_getter import get_labels_cnts
 
 sns.set(font_scale=1.5)
 
@@ -20,7 +24,7 @@ def learn_heatmap():
 def draw_heatmap(data):
     sns.set_context({"figure.figsize": (8, 8)})
     sns.heatmap(data=data, square=True)
-    plt.show()
+    plt.savefig('heatmap.svg', dpi=600, format='svg')
 
 
 def prob_log(label):
@@ -44,12 +48,15 @@ def calc_kl_divergence(client_names, label_tensors):
     return div_frame
 
 
-# learn_heatmap()
-client_names = ['guest', 'host']
-labels = [[100.0, 1000.0, 0, 0], [100.0, 999.0, 0.0, 20.0]]
+client_nums = 8
+
+server_path = '/data/projects/my_dataset'
+
+client_names = [f'client{i}' for i in range(1, client_nums + 1)]
+# 统计各个客户端的标签变量
+labels = [get_labels_cnts(os.path.join(server_path, f'client{i}/train')) for i in range(1, client_nums + 1)]
 
 div_frame = calc_kl_divergence(client_names=client_names, label_tensors=torch.Tensor(labels))
-print(div_frame)
 data = sns.load_dataset('flights') \
     .pivot('year', 'month', 'passengers')
 draw_heatmap(div_frame)
