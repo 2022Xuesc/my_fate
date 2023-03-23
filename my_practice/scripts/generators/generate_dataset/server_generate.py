d4c205968a2cb10a6639c105d9ed05d1ce138753
2022Xuesc <22S051026@stu.hit.edu.cn>
2023-03-23 10:35:22 +0800

    update

--- my_practice/scripts/generators/generate_dataset/server_generate.py
+++ my_practice/scripts/generators/generate_dataset/server_generate.py
@@ -1,8 +1,12 @@
+import os
+
 from dataset_generator import *
 
 train_path = "/data/projects/dataset/train2014"
 val_path = "/data/projects/dataset/val2014"
 test_path = "/data/projects/dataset/test2014"
+
+
 # train_path = "../../my_data/imagenet/guest/train/images"
 # val_path = "../../my_data/imagenet/guest/train/images"
 # test_path = "../../my_data/imagenet/guest/train/images"
@@ -25,6 +29,17 @@ def generate_dataset(n):
         generate_configs(path_list)
 
 
+# 先跳过测试数据集的划分
+def generate_labels_for_clusters(dir_path, client_num):
+    for i in range(1, client_num + 1):
+        client_path = os.path.join(dir_path, f'client{i}')
+        client_train_path = os.path.join(client_path, 'train')
+        client_val_path = os.path.join(client_path, 'val')
+        path_list = [client_train_path, client_val_path]
+        generate_labels(path_list)
+        generate_configs(path_list)
+
+
 # 生成全局的标注数据
 annotations_dir = "/data/projects/dataset/annotations"
 train_annotation_file = os.path.join(annotations_dir, 'instances_train2014.json')
@@ -37,4 +52,7 @@ test_annotation_file = os.path.join(annotations_dir, 'image_info_test2014.json')
 # save_image2labels(test_annotation_file, 'test')
 
 # 为每个文件夹生成标注数据以及config.yaml文件
-generate_dataset(8)
+# generate_dataset(8)
+
+clustered_dir = '/data/projects/clustered_dataset'
+generate_labels_for_clusters(clustered_dir,client_num=8)
