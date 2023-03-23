e3dff64be1dba37be30dc3408cb07f506e86c4db
2022Xuesc <22S051026@stu.hit.edu.cn>
2023-03-23 16:13:59 +0800

    update

---
+++ my_practice/scripts/multi-label/server/non-iid/bind_data/bind_all_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+flow table bind --drop -c bind_client1_train_path.json
+flow table bind --drop -c bind_client1_val_path.json
+flow table bind --drop -c bind_client2_train_path.json
+flow table bind --drop -c bind_client2_val_path.json
+flow table bind --drop -c bind_client3_train_path.json
+flow table bind --drop -c bind_client3_val_path.json
+flow table bind --drop -c bind_client4_train_path.json
+flow table bind --drop -c bind_client4_val_path.json
+flow table bind --drop -c bind_client5_train_path.json
+flow table bind --drop -c bind_client5_val_path.json
+flow table bind --drop -c bind_client6_train_path.json
+flow table bind --drop -c bind_client6_val_path.json
+flow table bind --drop -c bind_client7_train_path.json
+flow table bind --drop -c bind_client7_val_path.json
+flow table bind --drop -c bind_client8_train_path.json
+flow table bind --drop -c bind_client8_val_path.json
