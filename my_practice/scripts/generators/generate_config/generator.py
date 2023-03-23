5fa724fd4129b97eb1f1731c27daf464bc46c360
2022Xuesc <22S051026@stu.hit.edu.cn>
2023-03-23 16:13:59 +0800

    update

--- my_practice/scripts/generators/generate_config/generator.py
+++ my_practice/scripts/generators/generate_config/generator.py
@@ -1,10 +1,16 @@
 import json
 import os.path
 
+data_dir = '/data/projects/clustered_dataset'
+namespace = 'ms-coco-non-iid'
+type = 'non-iid'
+
 client_num = 8
 phases = ['train', 'val']
+dir_path = f"../../multi-label/server/{type}/bind_data"
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
 
-dir_path = "../../multi-label/server/bind_data"
 bind_name = os.path.join(dir_path, "bind_all_data.sh")
 f = open(bind_name, 'w')
 f.write("#!/bin/bash\n")
@@ -14,12 +20,13 @@ for i in range(1, client_num + 1):
         filename = f'bind_client{i}_{phase}_path.json'
         f.write(f"flow table bind --drop -c {filename}\n")
         file_path = os.path.join(dir_path, filename)
+        print(file_path)
         obj = {
             "engine": "PATH",
-            "namespace": "ms-coco-0321",
+            "namespace": namespace,
             "name": f"client{i}_{phase}",
             "address": {
-                "path": f"/data/projects/my_dataset/client{i}/{phase}"
+                "path": f"{data_dir}/client{i}/{phase}"
             }
         }
         json_str = json.dumps(obj, indent=4)
