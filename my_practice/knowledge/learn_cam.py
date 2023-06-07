af71774805b920c5bae85b9c147d7991a5a528df
Klaus125 <1367944566@qq.com>
2023-06-07 09:16:19 +0800

    update

--- my_practice/knowledge/learn_cam.py
+++ my_practice/knowledge/learn_cam.py
@@ -7,12 +7,10 @@ import matplotlib.pyplot as plt
 model = resnet18(pretrained=True).eval()
 img = read_image('dog.jpeg')
 input_tensor = normalize(resize(img, (224, 224)) / 255., [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-
-with SmoothGradCAMpp(model) as cam_extractor:
-    # 添加batch_size维
-    out = model(input_tensor.unsqueeze(0))
-    activation_map = cam_extractor(out.squeeze(0).argmax().item(), out)
-    plt.imshow(activation_map[0].squeeze(0).numpy())
-    plt.axis('off')
-    plt.tight_layout()
-    plt.show()
+cam_extractor = SmoothGradCAMpp(model)
+out = model(input_tensor.unsqueeze(0))
+activation_map = cam_extractor(out.squeeze(0).argmax().item(), out)
+plt.imshow(activation_map[0].squeeze(0).numpy())
+plt.axis('off')
+plt.tight_layout()
+plt.show()
