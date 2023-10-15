35415f9d0ec0f6739888a9478fde94b319756e72
2022Xuesc <22s051026@stu.hit.edu.cn>
2023-10-15 13:36:11 +0000

    update res

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -13,10 +13,10 @@
 import os
 import shutil
 
-dir_id = "202310141058527568340"
-job_id = "202310141058527568340"
+dir_id = "202310141100215339370"
+job_id = "202310141100215339370"
 module_name = 'multi_label_0'
-target_dir = 'voc/voc_fpsl'
+target_dir = 'voc/voc_interactive_40_onecycle'
 client_num = 10
 
 
@@ -44,6 +44,7 @@ def mv_stats(role, role_ids, target_dir):
 mv_stats('arbiter', 999, target_dir)
 mv_stats('guest', 10, target_dir)
 mv_stats('host', list(range(1, client_num)), target_dir)
+
 
 
 
