a5abc331965a1787dc4e1fd166ac5193ed2b0ab0
Klaus125 <1367944566@qq.com>
2023-12-28 20:05:29 +0800

    update

--- my_practice/experiment_res/IJCNN/voc/calculate_res.py
+++ my_practice/experiment_res/IJCNN/voc/calculate_res.py
@@ -11,7 +11,12 @@ def calculate_stats(float_list):
     return minimum, maximum, mean
 
 
-paths = ['agg_salgl', 'kmeans', 'salgl', 'c_gcn_with_agg', 'c_gcn_without_agg', 'p_gcn_with_agg', 'p_gcn_without_agg']
+# paths = ['agg_salgl', 'kmeans', 'salgl', 'c_gcn_with_agg', 'c_gcn_without_agg', 'p_gcn_with_agg', 'p_gcn_without_agg']
+all_paths = os.listdir()
+paths = []
+for path in all_paths:
+    if os.path.isdir(path):
+        paths.append(path)
 for path in paths:
     clients_path = [os.path.join(path, 'guest/10')]
     for i in range(1, 10):
