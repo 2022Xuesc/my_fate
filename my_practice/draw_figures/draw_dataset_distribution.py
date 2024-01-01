a7c33d717882b0c8e5108654d133955f81b24d50
2022Xuesc <22s051026@stu.hit.edu.cn>
2024-01-01 13:24:56 +0000

    update

--- my_practice/draw_figures/draw_dataset_distribution.py
+++ my_practice/draw_figures/draw_dataset_distribution.py
@@ -1,18 +1,20 @@
-import os
 import matplotlib.pyplot as plt
-import json
 import numpy as np
+
+import json
+import os
+
 
 def get_sample_size(data_dir):
     anno_path = os.path.join(data_dir, 'anno.json')
     anno = json.load(open(anno_path, 'r'))
     return len(anno)
 
+
 client_nums = 10
 
-# server_path = '/data/projects/clustered_dataset'
-
-server_path = 'anno_json_dir'
+# client_path = '/home/klaus125/research/fate/my_practice/dataset/coco/data/guest/train'
+server_path = '/data/projects/dataset/voc2007/clustered_voc_expanded'
 
 save_dir = 'clusters_distribution'
 
@@ -32,20 +34,9 @@ plt.figure(figsize=(12, 12))
 for i in range(client_nums):
     client_id = i + 1
     client_train_path = os.path.join(server_path, f'client{client_id}/train')
-    client_valid_path = os.path.join(server_path, f'client{client_id}/val')
-
-    # Todo: 作出分布直方图
-    # draw_hist(save_dir, [client_train_path, client_valid_path])
-
-    # labels = get_labels_cnts(client_train_path)
-
-    # total_labels.append(get_labels_feature(labels))
 
     # 只选取训练集大小
     samples.append(get_sample_size(client_train_path))
-
-# div_frame = calc_kl_divergence(client_names=client_names, label_tensors=torch.Tensor(total_labels))
-# draw_heatmap(save_dir, div_frame)
 
 plt.figure(figsize=(12, 12))
 # Todo: 作出关于samples的柱状图
@@ -60,3 +51,4 @@ plt.title('Distribution of MS-COCO datasets among clients')
 plt.ylabel('The size of the client dataset (log10)')
 
 plt.savefig(f'{save_dir}/dataset_distribution.svg', dpi=600, format='svg')
+
