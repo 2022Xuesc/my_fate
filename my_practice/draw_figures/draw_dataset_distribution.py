a04b5f6ac712ff5e3493aa2e43f74e0d37e5068e
2022Xuesc <22s051026@stu.hit.edu.cn>
2024-01-01 13:33:47 +0000

    update

--- my_practice/draw_figures/draw_dataset_distribution.py
+++ my_practice/draw_figures/draw_dataset_distribution.py
@@ -13,8 +13,13 @@ def get_sample_size(data_dir):
 
 client_nums = 10
 
+dataset = "VOC 2007"
 # client_path = '/home/klaus125/research/fate/my_practice/dataset/coco/data/guest/train'
-server_path = '/data/projects/dataset/voc2007/clustered_voc_expanded'
+
+if dataset == 'MS-COCO':
+    server_path = "/data/projects/dataset/clustered_dataset"
+else:
+    server_path = '/data/projects/dataset/voc2007/clustered_voc_expanded'
 
 save_dir = 'clusters_distribution'
 
@@ -28,8 +33,6 @@ total_labels = []
 # Todo: 记录每个客户端的样本数量，画出直方图
 samples = []
 
-plt.figure(figsize=(12, 12))
-
 # 画直方图
 for i in range(client_nums):
     client_id = i + 1
@@ -38,17 +41,16 @@ for i in range(client_nums):
     # 只选取训练集大小
     samples.append(get_sample_size(client_train_path))
 
-plt.figure(figsize=(12, 12))
 # Todo: 作出关于samples的柱状图
 log_samples = np.log10(samples)
 
 plt.bar(client_names, log_samples)
 
 for i, v in enumerate(log_samples):
-    plt.text(i, v + 0.05, str(samples[i]), ha='center')
+    plt.text(i, v + 0.01, str(samples[i]), ha='center')
 
-plt.title('Distribution of MS-COCO datasets among clients')
+plt.title(f'Distribution of {dataset} datasets among clients')
 plt.ylabel('The size of the client dataset (log10)')
 
-plt.savefig(f'{save_dir}/dataset_distribution.svg', dpi=600, format='svg')
+plt.savefig(f'{save_dir}/{dataset}_dataset_distribution.svg', dpi=600, format='svg')
 
