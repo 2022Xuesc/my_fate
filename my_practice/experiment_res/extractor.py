fe610cad99f4daf9476c53ef89c95474995195b9
2022Xuesc <22s051026@stu.hit.edu.cn>
2024-01-09 14:12:39 +0000

    update

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -13,8 +13,8 @@
 import os
 import shutil
 
-dir_id = "202401031247018492780"
-job_id = "202401031247018492780"
+dir_id = "202401080057225149450"
+job_id = "202401080057225149450"
 module_name = 'gcn_0'
 target_dir = 'IJCNN/c_gcn_no_agg'
 client_num = 10
