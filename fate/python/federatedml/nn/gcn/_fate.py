1b7818785e737b04946122c61b90e1d76769edb3
Klaus125 <1367944566@qq.com>
2024-05-22 23:37:43 +0800

    update

--- fate/python/federatedml/nn/gcn/_fate.py
+++ fate/python/federatedml/nn/gcn/_fate.py
@@ -259,12 +259,12 @@ def build_fitter(param: GCNParam, train_data, valid_data):
     category_dir = f'/data/projects/fate/my_practice/dataset/{dataset}'
 
     # Todo: [WARN]
-    #param.batch_size = 2
-    #param.max_iter = 1000
-    #param.num_labels = 20
-    #param.device = 'cuda:0'
-    #param.lr = 0.0001
-    #param.aggregate_every_n_epoch = 1
+    # param.batch_size = 2
+    # param.max_iter = 1000
+    # param.num_labels = 20
+    # param.device = 'cuda:0'
+    # param.lr = 0.0001
+    # param.aggregate_every_n_epoch = 1
 
     epochs = param.aggregate_every_n_epoch * param.max_iter
     context = FedClientContext(
