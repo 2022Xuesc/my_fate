8dfb1d4f136c0e261627091f895ec147befaff92
Klaus125 <1367944566@qq.com>
2024-06-05 10:46:29 +0800

    updateg

--- fate/python/federatedml/nn/backend/gcn/models/pruned_add_gcn/connect_add_gcn.py
+++ fate/python/federatedml/nn/backend/gcn/models/pruned_add_gcn/connect_add_gcn.py
@@ -50,7 +50,7 @@ class DynamicGraphConvolution(nn.Module):
         x = self.static_weight(x.transpose(1, 2))
         return x, adj  # 将静态相关性矩阵返回
 
-    def forward_construct_dynamic_graph(self, x,connect_vec):
+    def forward_construct_dynamic_graph(self, x, connect_vec):
         # Todo: 扩展后拼接
         connect_vec = connect_vec.unsqueeze(-1).expand(connect_vec.size(0), connect_vec.size(1), x.size(2))
         ### Construct the dynamic correlation matrix ###
@@ -67,7 +67,7 @@ class DynamicGraphConvolution(nn.Module):
         x = self.relu(x)
         return x
 
-    def forward(self, x,connect_vec, out1=None, prob=False, gap=False):
+    def forward(self, x, connect_vec, out1=None, prob=False, gap=False):
         """ D-GCN module
 
         Shape: 
@@ -77,13 +77,17 @@ class DynamicGraphConvolution(nn.Module):
         x = x.transpose(1, 2)
         out_static, static_adj = self.forward_static_gcn(x)
         x = x + out_static  # Todo: 残差连接
-        dynamic_adj = self.forward_construct_dynamic_graph(x,connect_vec)
+        dynamic_adj = self.forward_construct_dynamic_graph(x, connect_vec)
 
         num_classes = out1.size(1)
         dynamic_adj_loss = torch.tensor(0.).to(out1.device)
         # Todo: 概率向量损失不太好，怎么改进一下？
+        #   1. 改进1：out1不太稳定，对其进行sigmoid
+        #   2. 改进2：使用更加精细的控制代替矩阵乘法？之前的工作
+        out1 = nn.Sigmoid()(out1)
         if prob:
             transformed_out1 = torch.matmul(out1.unsqueeze(1), dynamic_adj).squeeze(1)
+            # Todo: 这里的话求过和，需要除以类别数量
             transformed_out1 /= num_classes
             # 第0维是batch_size，非对称损失也不求平均；因此，无需torch.mean
             dynamic_adj_loss += torch.sum(torch.norm(out1 - transformed_out1, dim=1))
@@ -121,12 +125,11 @@ class CONNECTED_ADD_GCN(nn.Module):
         self.prob = prob
         self.gap = gap
 
-
     def forward_feature(self, x):
         x = self.features(x)
         return x
 
-    def forward_dgcn(self, x,connect_vec, out1, prob, gap):
+    def forward_dgcn(self, x, connect_vec, out1, prob, gap):
         # Todo: 在这里调用，在这里配置
         x, dynamic_loss = self.gcn(x, connect_vec, out1, prob, gap)
         return x, dynamic_loss
@@ -137,10 +140,10 @@ class CONNECTED_ADD_GCN(nn.Module):
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         out1 = self.fc(x)
-        
+
         connect_vec = self.connect(x)
         # Todo: 第一维应该是batch，这里将二维特征展平
-        z, dynamic_adj_loss = self.forward_dgcn(inp,connect_vec, out1, self.prob, self.gap)
+        z, dynamic_adj_loss = self.forward_dgcn(inp, connect_vec, out1, self.prob, self.gap)
         z = z.transpose(1, 2)
 
         # output = torch.cat([z, x], dim=-1)
