64af036e85709e19914697ef91fdda36f642264f
Klaus125 <1367944566@qq.com>
2024-07-06 23:20:30 +0800

    update

--- fate/python/federatedml/nn/backend/gcn/models/papers/AAAI/gin.py
+++ fate/python/federatedml/nn/backend/gcn/models/papers/AAAI/gin.py
@@ -1,14 +1,40 @@
+import numpy as np
+import torch
+import torch.nn as nn
 from torch.nn import Parameter
 
-from federatedml.nn.backend.gcn.models.gin.gin_layer import GINLayer
-from federatedml.nn.backend.gcn.utils import *
+
+# 定义GIN层
+class GINLayer(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(GINLayer, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.mid_features = out_features * 2
+
+        self.fc1 = torch.nn.Linear(self.in_features, self.mid_features, bias)
+        self.fc2 = torch.nn.Linear(self.mid_features, self.out_features, bias)
+
+        self.bn = nn.BatchNorm1d(self.out_features)
+
+        # epsilon初始化为0，需要进行优化
+        self.epsilon = Parameter(torch.zeros(1), requires_grad=True)
+
+    # 前向传播，权重与输入相乘、结果再与邻接矩阵adj相乘。
+    def forward(self, x, adj):
+        # adj在左边，adj是转置后的[a11,a21,a31...]
+        x = (1 + self.epsilon) * x + torch.matmul(adj, x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.bn(x)
+        return x
 
 
-class GINResnet(nn.Module):
+class AAAI_GIN(nn.Module):
     def __init__(self, model, num_classes, in_channels=300, out_channels=1024,
-                 latent_dim=512, adjList=None,inp=None):
-        super(GINResnet, self).__init__()
-        self.A = self.generateA(adjList)
+                 latent_dim=512, adjList=None, inp=None):
+        super(AAAI_GIN, self).__init__()
+        self.A = self.generateA(adjList)  
         # 定义特征提取部分的网络
         self.features = nn.Sequential(
             model.conv1,
@@ -22,16 +48,14 @@ class GINResnet(nn.Module):
         )
 
         self.num_classes = num_classes
-        # Todo: 这里使用MaxPool2d好还是AdaptivePool好？之后进行验证
-        # self.pooling = nn.MaxPool2d(14, 14)
-        self.pooling = nn.AdaptiveAvgPool2d((1,1))
+        self.pooling = nn.AdaptiveAvgPool2d((1, 1))
 
         # 定义图同构层
         self.gin1 = GINLayer(in_channels, out_channels)
-        # Todo: 为了建立残差连接，将两个gin层的输出维度调成一样的
         self.gin2 = GINLayer(out_channels, out_channels)
         self.inp = inp
         # 定义重要性向量层
+        # Todo: 考虑对这里的层进行修改
         self.imp_layer = nn.Linear(out_channels, latent_dim, True)
         self.latent_layer = nn.Linear(2048, latent_dim, True)
         self.map_layer = nn.Linear(latent_dim, latent_dim, True)
@@ -43,10 +67,14 @@ class GINResnet(nn.Module):
         self.classifier = nn.Linear(latent_dim, 1, True)
 
     def generateA(self, adjList):
-        return Parameter(torch.from_numpy(adjList.astype(np.float32)))
+        adj = torch.from_numpy(adjList.astype(np.float32))
+        adj = torch.transpose(adj, 0, 1)
+        return Parameter(adj)
 
     def updateA(self, adjList):
-        self.A.data.copy_(torch.from_numpy(adjList).float())
+        adj = torch.from_numpy(adjList).float()
+        adj = torch.transpose(adj, 0, 1)
+        self.A.data.copy_(adj)
 
     # 前向传播逻辑：features --> pooling -->
     def forward(self, feature, inp):
@@ -54,11 +82,11 @@ class GINResnet(nn.Module):
         feature = self.pooling(feature)
         feature = feature.view(feature.size(0), -1)
 
-        inp = inp[0] if self.inp is None else self.inp # 从数据集中获取初始标签嵌入
+        inp = inp[0] if self.inp is None else self.inp  # 从数据集中获取初始标签嵌入
         x = self.gin1(inp, self.A)
         x = self.relu(x)
 
-        identity = x                # 添加残差连接
+        identity = x  # 添加残差连接
         x = self.gin2(x, self.A)
 
         x = self.relu(x + identity)
