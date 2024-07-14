706f7eb89e042b623c6dedd1404fdb35a92455eb
2022Xuesc <22s051026@stu.hit.edu.cn>
2024-07-14 07:38:46 +0000

    update

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -13,10 +13,10 @@
 import os
 import shutil
 
-dir_id = "202407101557060365940"
-job_id = "202407101557060365940"
+dir_id = "202406261611099073290"
+job_id = "202406261611099073290"
 module_name = 'gcn_0'
-target_dir = 'AAAI/coco/add_gap_gcn'
+target_dir = 'work0/dynamic_ratio'
 client_num = 10
 
 
@@ -45,6 +45,7 @@ def mv_stats(role, role_ids, target_dir):
 mv_stats('arbiter', 999, target_dir)
 mv_stats('guest', 10, target_dir)
 mv_stats('host', list(range(1, client_num)), target_dir)
+
 
 
 
