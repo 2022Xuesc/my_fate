845fc79c2ce5d84f7500445f47b0d318fb48f40b
Klaus125 <1367944566@qq.com>
2024-07-28 13:14:02 +0800

    update

--- fate/python/federatedml/nn/backend/gcn/models/__init__.py
+++ fate/python/federatedml/nn/backend/gcn/models/__init__.py
@@ -27,6 +27,8 @@ from federatedml.nn.backend.gcn.models.papers.AAAI_FIXED.fixed_connect_add_resid
 from federatedml.nn.backend.gcn.models.papers.AAAI_FIXED.fixed_connect_prob_gcn import AAAI_FIXED_CONNECT_PROB_GCN
 from federatedml.nn.backend.gcn.models.papers.AAAI_FIXED.fixed_connect_prob_residual_gcn import \
     AAAI_FIXED_CONNECT_PROB_RESIDUAL_GCN
+from federatedml.nn.backend.gcn.models.papers.AAAI_FIXED.fixed_connect_standard_gcn import \
+    AAAI_FIXED_CONNECT_STANDARD_GCN
 from federatedml.nn.backend.gcn.models.papers.IJCNN.Agg_SALGL.resnet_agg_salgl import ResnetAggSalgl
 from federatedml.nn.backend.gcn.models.papers.IJCNN.GCN.c_gcn import ResnetCGCN
 from federatedml.nn.backend.gcn.models.papers.IJCNN.GCN.p_gcn import ResnetPGCN
@@ -212,6 +214,14 @@ def aaai_fixed_add_standard_gcn(pretrained, adjList, device='cpu', num_classes=8
     model = torch_models.resnet101(pretrained)
     model = AAAI_FIXED_ADD_STANDARD_GCN(model, num_classes, in_channels, out_channels, adjList,
                                         needOptimize=needOptimize)
+    return model.to(device)
+
+
+def aaai_fixed_connect_standard_gcn(pretrained, adjList, device='cpu', num_classes=80, in_channels=1024,
+                                    out_channels=2048, prob=True, gap=False, needOptimize=True):
+    model = torch_models.resnet101(pretrained)
+    model = AAAI_FIXED_CONNECT_STANDARD_GCN(model, num_classes, in_channels, out_channels, adjList,
+                                            )
     return model.to(device)
 
 
