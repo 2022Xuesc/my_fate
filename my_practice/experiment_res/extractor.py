503f727b688248341c027290d7d429d7d3bb8619
2022Xuesc <22s051026@stu.hit.edu.cn>
2024-07-28 14:48:20 +0000

    add experiment_res

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -13,10 +13,10 @@
 import os
 import shutil
 
-dir_id = "202407281033362860880"
-job_id = "202407281033362860880"
+dir_id = "202407281257225264050"
+job_id = "202407281257225264050"
 module_name = 'gcn_0'
-target_dir = 'AAAI/voc2012/fixed_connect_prob_standard_residual_gcn'
+target_dir = 'AAAI/voc2007/fixed_connect_prob_standard_gcn'
 client_num = 10
 
 
@@ -45,6 +45,7 @@ def mv_stats(role, role_ids, target_dir):
 mv_stats('arbiter', 999, target_dir)
 mv_stats('guest', 10, target_dir)
 mv_stats('host', list(range(1, client_num)), target_dir)
+
 
 
 
