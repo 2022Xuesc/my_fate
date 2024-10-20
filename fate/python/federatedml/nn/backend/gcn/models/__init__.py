f526444cd0fed7888df95ae6b3787a0ca5afc8be
Klaus125 <1367944566@qq.com>
2024-10-20 11:44:56 +0800

    update

--- fate/python/federatedml/nn/backend/gcn/models/__init__.py
+++ fate/python/federatedml/nn/backend/gcn/models/__init__.py
@@ -1,5 +1,5 @@
 import torchvision.models as torch_models
-
+import torch
 from federatedml.nn.backend.gcn.models.full_salgl import FullSALGL
 from federatedml.nn.backend.gcn.models.gin.ml_gin import GINResnet
 from federatedml.nn.backend.gcn.models.instance_gcn.add_gcn import ADD_GCN
@@ -232,6 +232,7 @@ def aaai_fixed_connect_standard_gcn(pretrained, adjList, device='cpu', num_class
 def aaai_fixed_connect_prob_standard_gcn(pretrained, adjList, device='cpu', num_classes=80, in_channels=1024,
                                          out_channels=2048, prob=True, gap=False, needOptimize=True):
     model = torch_models.resnet101(pretrained)
+    # model = torch.load('/data/projects/models/resnet101-init.pth')
     model = AAAI_FIXED_CONNECT_PROB_STANDARD_GCN(model, num_classes, in_channels, out_channels, adjList,
                                                  )
     return model.to(device)
