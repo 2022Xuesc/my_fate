ff977cbc09ed889b6a5ba4e7a736883395c5e2a6
Klaus125 <1367944566@qq.com>
2024-10-21 21:39:38 +0800

    update

--- fate/python/federatedml/nn/gcn/_fate.py
+++ fate/python/federatedml/nn/gcn/_fate.py
@@ -258,8 +258,8 @@ def build_fitter(param: GCNParam, train_data, valid_data):
     # param.lr = 0.0001
     # param.aggregate_every_n_epoch = 1
 
-    category_dir = '/data/projects/fate/my_practice/dataset/voc2012/'
-    # category_dir = '/home/klaus125/research/fate/my_practice/dataset/voc2012'
+    category_dir = '/data/projects/fate/my_practice/dataset/voc_expanded'
+    # category_dir = '/home/klaus125/research/fate/my_practice/dataset/voc_expanded'
 
     epochs = param.aggregate_every_n_epoch * param.max_iter
     context = FedClientContext(
@@ -268,7 +268,7 @@ def build_fitter(param: GCNParam, train_data, valid_data):
     )
     # 与服务器进行握手
     context.init()
-    inp_name = 'voc2012_glove_word2vec.pkl'
+    inp_name = 'voc_expanded_glove_word2vec.pkl'
     # 构建数据集
 
     batch_size = param.batch_size
