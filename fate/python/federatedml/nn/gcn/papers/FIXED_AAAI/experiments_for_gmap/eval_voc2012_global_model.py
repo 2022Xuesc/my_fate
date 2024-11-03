c6f41678f7f847d69fb1de06eabb4db09d2629cb
2022Xuesc <22S051026@stu.hit.edu.cn>
2024-11-03 15:16:28 +0000

    update

--- fate/python/federatedml/nn/gcn/papers/FIXED_AAAI/experiments_for_gmap/eval_voc2012_global_model.py
+++ fate/python/federatedml/nn/gcn/papers/FIXED_AAAI/experiments_for_gmap/eval_voc2012_global_model.py
@@ -28,7 +28,7 @@ jobid_map = {
     # FPSL: '202410220819086555390',
     # C_GCN: '202410220904548901360',
     # P_GCN: '202410221551230972550',
-    WITHOUT_FIX: '202410250218416948480',
+    # WITHOUT_FIX: '202410250218416948480',
     WITHOUT_CONNECT: '202410250650256294940',
 }
 model_map = {
@@ -103,7 +103,7 @@ cur_dir_name = os.getcwd()
 my_writer = MyWriter(dir_name=cur_dir_name, stats_name='voc2012_stats')
 
 for task_name in jobid_map:
-    is_multi_label = task_name.startswith('f')
+    is_multi_label = task_name.startswith('f') and not task_name.startswith('fixed')
     jobid = jobid_map[task_name]
     if len(jobid) == 0:
         continue
@@ -236,3 +236,4 @@ for task_name in jobid_map:
         loss = losses[OBJECTIVE_LOSS_KEY].mean
         valid_writer.writerow([i, mAP.item(), loss])
         valid_aps_writer.writerow(ap)
+
