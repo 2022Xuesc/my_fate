0f25f08cd667b198f1a271aae8a7f004b658873d
Klaus125 <1367944566@qq.com>
2024-11-05 20:05:34 +0800

    update

--- my_practice/draw_figures/draw_performance_curve.py
+++ my_practice/draw_figures/draw_performance_curve.py
@@ -4,16 +4,25 @@ from pandas import Series
 import csv
 import os
 
-base_dir = '../experiment_res/AAAI/coco'
-methods = ['fedavg', 'flag', 'fpsl', 'c_gcn', 'p_gcn', 'fixed_connect_prob_standard_gcn']
+dataset = 'coco'
+base_dir = f'../experiment_res/AAAI/{dataset}'
+methods = [
+    'fedavg',
+    # 'flag',
+    'fpsl',
+    'c_gcn',
+    'p_gcn',
+    'fixed_connect_prob_standard_gcn']
 
-colors = ['g', 'b', 'r', 'y', 'purple', 'brown']
+# colors = ['g', 'b', 'r', 'palegreen', 'purple', 'gold']
 
 AmAP_list = dict()
 
 show_epochs = 100000
 for method in methods:
     path = os.path.join(base_dir, method)
+    if not os.path.exists(path):
+        continue
     clients_path = [os.path.join(path, 'guest/10')]
     for i in range(1, 10):
         clients_path.append(os.path.join(path, f'host/{i}'))
@@ -25,7 +34,7 @@ for method in methods:
             min_epoch = min(min_epoch, len(reader_list))
             mAPs = []
             for j in range(len(reader_list)):
-                cur_mAP = float(reader_list[j].get('mAP'))
+                cur_mAP = float(reader_list[j].get('map'))
                 mAPs.append(cur_mAP)
             mAP_lists.append(mAPs)
     show_epochs = min(show_epochs, min_epoch)
@@ -42,10 +51,14 @@ x_axis = 'epoch'
 
 for i in range(len(methods)):
     method = methods[i]
-    plt.plot(x_series, Series(AmAP_list[method][0:show_epochs]), colors[i])
+    if i == len(methods) - 1:
+        plt.plot(x_series, Series(AmAP_list[method][0:show_epochs]), color='b')
+    else:
+        plt.plot(x_series, Series(AmAP_list[method][0:show_epochs]))
 plt.xlabel(x_axis)
-plt.ylabel('valid mAP')
+plt.ylabel('AmAP')
 plt.legend(methods[0:5] + ['ours'])
 plt.title('The relation between AmAP and total epochs.')
 
-plt.show()
+save_path = os.path.join('convergence_res', f'res_on_{dataset}.svg')
+plt.savefig(save_path, dpi=600, format='svg')
