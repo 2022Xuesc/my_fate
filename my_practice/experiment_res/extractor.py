ca1151843f8742e1f174b4e017b718c6f71af7f6
2022Xuesc <22S051026@stu.hit.edu.cn>
2024-11-09 10:59:49 +0000

    update

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -4,10 +4,10 @@
 import os
 import shutil
 
-dir_id = "202411070650066368740"
-job_id = "202411070650066368740"
-module_name = 'gcn_0'
-target_dir = 'AAAI/coco2017/ours'
+dir_id = "202411081213231969310"
+job_id = "202411081213231969310"
+module_name = 'multi_label_0'
+target_dir = 'AAAI/coco2017/fed_avg'
 client_num = 10
 
 
@@ -36,6 +36,7 @@ def mv_stats(role, role_ids, target_dir):
 mv_stats('arbiter', 999, target_dir)
 mv_stats('guest', 10, target_dir)
 mv_stats('host', list(range(1, client_num)), target_dir)
+
 
 
 
