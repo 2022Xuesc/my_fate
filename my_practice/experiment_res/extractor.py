fdbbd58dd03d7c8793ad09d0328b01457694af57
2022Xuesc <22S051026@stu.hit.edu.cn>
2024-11-18 02:51:43 +0000

    update

--- my_practice/experiment_res/extractor.py
+++ my_practice/experiment_res/extractor.py
@@ -4,10 +4,10 @@
 import os
 import shutil
 
-dir_id = "202411161045471466580"
-job_id = "202411161045471466580"
+dir_id = "202411161322167998130"
+job_id = "202411161322167998130"
 module_name = 'gcn_0'
-target_dir = 'AAAI/coco2017/p_gcn_new'
+target_dir = 'AAAI/coco2017/c_gcn_new'
 client_num = 10
 
 
@@ -36,6 +36,7 @@ def mv_stats(role, role_ids, target_dir):
 mv_stats('arbiter', 999, target_dir)
 mv_stats('guest', 10, target_dir)
 mv_stats('host', list(range(1, client_num)), target_dir)
+
 
 
 
