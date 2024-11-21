4d6192a217b60bd4f217a4b040ec4a1f76a2f461
2022Xuesc <22S051026@stu.hit.edu.cn>
2024-11-21 14:09:28 +0000

    update

--- my_practice/draw_figures/draw_dataset_distribution.py
+++ my_practice/draw_figures/draw_dataset_distribution.py
@@ -34,7 +34,7 @@ for dataset in datasets:
         server_path = '/data/projects/dataset/clustered_voc2012'
     else:
         server_path = '/data/projects/dataset/coco2017/clustered_dataset'
-    save_dir = 'clusters_distribution'
+    save_dir = 'new_distribution'
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
@@ -65,8 +65,8 @@ for dataset in datasets:
         plt.text(i, v + 0.03, str(samples[i]), ha='center')
 
     # plt.title(f'Distribution of {dataset} datasets among clients')
-    # plt.ylabel('客户端数据集大小（log10）', fontproperties=font)
-    plt.ylabel('The size of the client dataset(log10)')
+    plt.ylabel('客户端数据集大小（log10）', fontproperties=font)
+    # plt.ylabel('The size of the client dataset(log10)')
 
     plt.savefig(f'{save_dir}/{dataset}_dataset_distribution.svg', dpi=600, format='svg')
     plt.close()
