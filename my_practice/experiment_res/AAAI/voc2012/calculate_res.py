6d1f572e94218e766c1a61a19a1cf7d7436d25d7
2022Xuesc <22S051026@stu.hit.edu.cn>
2024-12-12 05:52:01 +0000

    update

--- my_practice/experiment_res/AAAI/voc2012/calculate_res.py
+++ my_practice/experiment_res/AAAI/voc2012/calculate_res.py
@@ -17,7 +17,7 @@ def calculate_stats(float_list):
 # for path in all_paths:
 #     if os.path.isdir(f'stats/{path}') and not path.startswith('compare'):
 #         paths.append(f'stats/{path}')
-paths = ['c_gcn', 'p_gcn', 'fixed_connect_prob_standard_gcn', 'ours']
+paths = ['add_gcn_large_lr','fixed_connect_prob_standard_gcn']
 
 for path in paths:
     if not os.path.isdir(path):
